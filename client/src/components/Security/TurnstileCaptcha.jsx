3aebb8a6fcb7d63ad35df962d2954df1875cd1d3
pradhappn <pradhappns222@gmail.com>
2026-07-16 16:04:16 +0530

    code updated

--- client/src/components/Security/TurnstileCaptcha.jsx
+++ client/src/components/Security/TurnstileCaptcha.jsx
@@ -1,6 +1,19 @@
-import Turnstile from "react-turnstile";
+import { useEffect } from "react";
+import { Turnstile } from "react-turnstile";
 
 export default function TurnstileCaptcha({ setToken }) {
+  const isDevelopment = import.meta.env.DEV;
+
+  useEffect(() => {
+    if (isDevelopment) {
+      setToken("local-development-token");
+    }
+  }, [isDevelopment, setToken]);
+
+  if (isDevelopment) {
+    return <small>Security verification is disabled in local development.</small>;
+  }
+
   return (
     <Turnstile
       sitekey={import.meta.env.VITE_TURNSTILE_SITE_KEY}

899ab64b06c0391e5e17e9a20ca0b43053c5b5d8
sabari0817 <a.sabarinathansabari08@gmail.com>
2026-07-21 21:48:48 +0530

    bug fixing

--- server/booking/settings.py
+++ server/booking/settings.py
@@ -29,6 +29,11 @@ SECRET_KEY = os.getenv("SECRET_KEY")
 DEBUG = os.getenv("DEBUG", "False") == "True"
 ALLOWED_HOSTS = os.getenv("ALLOWED_HOSTS", "127.0.0.1,localhost").split(",")
 
+CSRF_TRUSTED_ORIGINS = [
+    "http://localhost:5173",
+    "https://ticket-requests.onrender.com",
+]
+
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:5173",
     "http://127.0.0.1:5173",

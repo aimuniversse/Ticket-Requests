7944a8706738b2a6775ee327dd96e566cd98f880
sabari0817 <a.sabarinathansabari08@gmail.com>
2026-07-21 20:53:27 +0530

    Fix PostgreSQL migration syntax

--- server/operators/migrations/0002_create_missing_wallet_transaction.py
+++ server/operators/migrations/0002_create_missing_wallet_transaction.py
@@ -11,22 +11,25 @@ class Migration(migrations.Migration):
         migrations.RunSQL(
             sql="""
                 CREATE TABLE IF NOT EXISTS "operators_transaction" (
-                    "id" integer NOT NULL PRIMARY KEY AUTOINCREMENT,
+                    "id" SERIAL PRIMARY KEY,
                     "transaction_type" varchar(10) NOT NULL,
                     "credits" integer NOT NULL,
                     "balance_after_transaction" integer NOT NULL,
                     "description" varchar(255) NOT NULL,
-                    "created_at" datetime NOT NULL,
+                    "created_at" timestamp NOT NULL,
                     "operator_id" bigint NOT NULL REFERENCES "operators_operator" ("id") DEFERRABLE INITIALLY DEFERRED
                 );
+
                 CREATE TABLE IF NOT EXISTS "operators_wallet" (
-                    "id" integer NOT NULL PRIMARY KEY AUTOINCREMENT,
+                    "id" SERIAL PRIMARY KEY,
                     "current_balance" integer NOT NULL CHECK ("current_balance" >= 0),
-                    "created_at" datetime NOT NULL,
-                    "updated_at" datetime NOT NULL,
+                    "created_at" timestamp NOT NULL,
+                    "updated_at" timestamp NOT NULL,
                     "operator_id" bigint NOT NULL UNIQUE REFERENCES "operators_operator" ("id") DEFERRABLE INITIALLY DEFERRED
                 );
-                CREATE INDEX IF NOT EXISTS "operators_transaction_operator_id_2b875156" ON "operators_transaction" ("operator_id");
+
+                CREATE INDEX IF NOT EXISTS "operators_transaction_operator_id_2b875156"
+                ON "operators_transaction" ("operator_id");
             """,
             reverse_sql=migrations.RunSQL.noop,
         ),
